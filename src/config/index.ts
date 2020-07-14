95cc7a0e5b3d12e6c60b51f0c295408c290f9d31
nissaka <maigechuang115@163.com>
2020-07-14 16:08:52 +0800

    127

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || "development";
 const debug = env !== "production";
 const port = process.env.PORT || (env === "production" ? 5001 : 5001);
-const host = process.env.HOST || `0.0.0.0:${port}`;
+const host = process.env.HOST || `127.0.0.1:${port}`;
 
 export default {
   env: env,
